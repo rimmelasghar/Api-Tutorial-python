84e4428e86058d2b966d20bc6b065e0305a9137a
☙ Sohlstyce ❧ <57762641+Sohlstyce@users.noreply.github.com>
2022-08-30 18:29:23 +0530

    Comment out a few things for clarity

--- Api.py
+++ Api.py
@@ -2,10 +2,15 @@
 import requests
 import json
 
-response_API = requests.get("https://api.covid19india.org/state_district_wise.json")
 
-print(response_API.status_code)
-data = response_API.text
-parse_json = json.loads(data)
-active_case = parse_json['Andaman and Nicobar Islands']['districtData']['South Andaman']['active']
-print("Active cases in South Andaman:", active_case)
+# A get request
+def Get_example():
+    response_API = requests.get("https://api.covid19india.org/state_district_wise.json") # Establishing a connection to the API
+
+    print(response_API.status_code) # Printing the status code. A status code is the indication whether the "request" was successful or not
+    data = response_API.text # Convert the response into readable text ouput
+    parse_json = json.loads(data) #Parse the data
+    active_case = parse_json['Andaman and Nicobar Islands']['districtData']['South Andaman']['active'] # Get specified data. This is basically like a directory.
+    print("Active cases in South Andaman:", active_case) # Print the output
+
+Get_example()
